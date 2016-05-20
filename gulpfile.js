9bcfd5f08ade1a40ea015d2ca7fae69dce590f7e
xottabbich@mail.ru <xottabbich@mail.ru>
2016-05-20 21:46:05 +0600

    Add autoprefixer in gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const gulp = require('gulp');
-var sass = require('gulp-sass');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function(){
   return gulp.src('scss/**/*.*')
     .pipe(sass())
+    .pipe(autoprefixer({
+			browsers: ['last 5 versions'],
+			cascade: false
+		}))
     .pipe(gulp.dest('css'));
 });
